167460e6e7c1b7bd6b8ad9d88d8506482f08d3bf
Yan <92958138+YanRoncati@users.noreply.github.com>
2021-10-21 23:31:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'YanRoncati',
+  youtube: 'Yan',
+  facebook: 'YanRoncati',
+  instagram: 'YanRoncati',
+  twitter: 'CansativoE'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      bio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
